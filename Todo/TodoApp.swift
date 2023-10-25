906ff620153f18b441557b407812cdcc945ebc26
Nicolay Kjærnet <nicolay.webdev@gmail.com>
2023-10-25 13:06:13 +0200

    Init
    First commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Nicolay Kjærnet on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
